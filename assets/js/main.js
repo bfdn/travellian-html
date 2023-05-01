3e6cbf6eca91e551a5c22c52c88396389a7aace6
Bilal Fidan <bilalfiddan@gmail.com>
2023-05-01 11:19:01 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,111 @@
+$('.destinations-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: false,
+    nav: true,
+    loop: false,
+    //margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        992: {
+            items: 3
+        },
+    }
+});
+
+$('.offer-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: false,
+    nav: true,
+    loop: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        992: {
+            items: 3
+        },
+        /*1200: {
+            items: 3,
+            loop: false
+        }*/
+    }
+});
+
+$('.trip-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: false,
+    nav: true,
+    loop: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        992: {
+            items: 2
+        },
+    }
+});
+
+$('.gallery-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: false,
+    nav: true,
+    loop: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        992: {
+            items: 3
+        },
+        1400: {
+            items: 4
+        },
+    }
+});
+
+
+$('.comments-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: false,
+    nav: true,
+    loop: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        992: {
+            items: 2
+        },
+        1400: {
+            items: 3
+        },
+    }
+});
